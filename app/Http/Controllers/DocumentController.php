324fb818838cf5b72d836dd9c6c8409689485961
Asfandyarkhanafridi <masfandy9@gmail.com>
2023-10-06 17:19:35 +0500

    a

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -34,7 +34,8 @@ class DocumentController extends Controller
         $departments = Department::all();
         $users = User::all();
         $files = Files::all();
-        return view('documents.create', compact('classifications','documentTypes', 'files', 'departments', 'users', 'authorizedUsers'));
+        $documents = Document::all();
+        return view('documents.create', compact('classifications','documentTypes', 'files', 'departments', 'users', 'authorizedUsers','documents'));
     }
 
     public function store(StoreDocumentRequest $request)
@@ -76,6 +77,13 @@ class DocumentController extends Controller
                     'document_id' => $document->id,
                     'userID' => $userID,
                 ]);
+            }
+
+            if($request->reference){
+                dd('this is reference',$request->reference);
+            }
+            elseif($request->reference_id){
+                dd('this is reference ID',$request->reference_id);
             }
 
             if ($request->name) {
