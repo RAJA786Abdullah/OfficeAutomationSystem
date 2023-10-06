0882417f460e3cddcb503f3e01a42296ab762744
Asfandyarkhanafridi <masfandy9@gmail.com>
2023-10-06 15:36:34 +0500

    a

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -17,19 +17,17 @@ class RoleSeeder extends Seeder
     {
         $aryRoles = [
             ['roleID' => 1, 'roleName' => 'Admin', 'description' => 'Role for Admin'],
-//            ['roleID' => 2, 'roleName' => 'User', 'description' => 'Role for User'],
-//
-//            ['roleID' => 3, 'roleName' => 'Clark IT', 'description' => 'clark it'],
-//            ['roleID' => 4, 'roleName' => 'Manager IT', 'description' => 'Manager it'],
-//            ['roleID' => 5, 'roleName' => 'Assistant Director IT', 'description' => 'assistant director it'],
-//            ['roleID' => 6, 'roleName' => 'Director IT', 'description' => 'director it'],
-//
-//            ['roleID' => 7, 'roleName' => 'Clark Land', 'description' => 'clark land'],
-//            ['roleID' => 8, 'roleName' => 'Manager Land', 'description' => 'Manager land'],
-//            ['roleID' => 9, 'roleName' => 'Assistant Director Land', 'description' => 'assistant director land'],
-//            ['roleID' => 10, 'roleName' => 'Director Land', 'description' => 'director land'],
+            ['roleID' => 2, 'roleName' => 'Director IT', 'description' => 'Role for Director IT'],
+            ['roleID' => 3, 'roleName' => 'Deputy Director IT', 'description' => 'Role for Deputy Director IT'],
+            ['roleID' => 4, 'roleName' => 'Assistant Director IT', 'description' => 'Role for Assistant Director IT'],
+            ['roleID' => 5, 'roleName' => 'Manager IT', 'description' => 'Role for Manager IT'],
+            ['roleID' => 6, 'roleName' => 'Clerk IT', 'description' => 'Role for Clerk IT'],
 
-
+            ['roleID' => 7, 'roleName' => 'Clerk Legal', 'description' => 'Role for Clerk Legal'],
+            ['roleID' => 8, 'roleName' => 'Manager Legal', 'description' => 'Role for Manager Legal'],
+            ['roleID' => 9, 'roleName' => 'Assistant Director Legal', 'description' => 'Role for Assistant Director Legal'],
+            ['roleID' => 10, 'roleName' => 'Deputy Director Legal', 'description' => 'Role for Deputy Director Legal'],
+            ['roleID' => 11, 'roleName' => 'Director Legal', 'description' => 'Role for Director Legal'],
         ];
 
 	    foreach ($aryRoles as $role) {
