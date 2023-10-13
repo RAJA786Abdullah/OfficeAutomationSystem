77fc4659dab0f2c069a7874f5524d5b19a77ac34
Asfandyarkhanafridi <masfandy9@gmail.com>
2023-10-13 09:40:28 +0500

    a

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,19 +11,17 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-
         $aryUsers = [
-            ['userID' => '1','name' => 'Admin','email' => 'admin','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$OSefiiwsNO20/61qW.L7xOwtaxqsj6/fZ5JkAnakXJTsHFwihSItu','department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => NULL],
-            ['userID' => '2','name' => 'Imran Qureshi','email' => 'hameed','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$Bh51gNJB0h2/B6k/Wyg5R.F/fsQPSKVTv3r/4UVjxwjWpTznMfG7O','department_id' => '1','branch_id' => '1','is_signing_authority' => '1','arm_designation' => 'Lt Col'],
-            ['userID' => '3','name' => 'Adnan Chaudhry','email' => 'adnan','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$mrRwyKT5ajAnYAw4T4FFdeXNh3RzQOxE76z792UlUu4s4s.mnWSya','department_id' => '1','branch_id' => '1','is_signing_authority' => '1','arm_designation' => 'civil'],
-            ['userID' => '4','name' => 'Abdullah','email' => 'abdullah','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$F96Nyw2ffsKJTyQYzI9WTu0t8kHH1FgTfHKfWQjYmcNYKSgZeBcDm','department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => 'civil'],
-            ['userID' => '5','name' => 'Umar Khan','email' => 'umarkhan','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$y/wYvzhGPHwa1FSp2opedep3GK/nJkzTYHy8K9NS688NhEBX.1bJW','department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => 'civil'],
-            ['userID' => '6','name' => 'Kashif','email' => 'kashif','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$pHr2X5LsxYd7o/olNnmPl.60BEBOpDP.Bdawbidk70m4MT31hqcQa','department_id' => '1','branch_id' => '1','is_signing_authority' => '1','arm_designation' => 'civil'],
-            ['userID' => '7','name' => 'Khuram','email' => 'khuram','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$H/StYiSVX6gXePXvcIrTMuLeYKLu7/HLojUODqhRS.RbD/98/keyS','department_id' => '1','branch_id' => '1','is_signing_authority' => '1','arm_designation' => 'civil'],
-            ['userID' => '8','name' => 'Wahab','email' => 'wahab','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$nmp7Wy8EMH4C0wKnlYXkXub3o5olkIiMOwjIwTK5vdWiv/YeJd8ru','department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => 'civil'],
-            ['userID' => '9','name' => 'Rahim','email' => 'rahim','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => '$2y$10$TfsGsNaH5G3Gofj3viYe0eFsiCu9lZS36mn/i6ZqvPIBpWjMEXZlO','department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => 'civil']
+            ['userID' => '1','name' => 'Admin','email' => 'admin','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => NULL],
+            ['userID' => '2','name' => 'Imran Qureshi','email' => 'imran','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '1','arm_designation' => 'Lt Col'],
+            ['userID' => '3','name' => 'Adnan Chaudhry','email' => 'adnan','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '1','arm_designation' => 'civil'],
+            ['userID' => '4','name' => 'Abdullah','email' => 'abdullah','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => 'civil'],
+            ['userID' => '5','name' => 'Umar Khan','email' => 'umar','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => 'civil'],
+            ['userID' => '6','name' => 'Kashif','email' => 'kashif','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '1','arm_designation' => 'civil'],
+            ['userID' => '7','name' => 'Khuram','email' => 'khuram','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '1','arm_designation' => 'civil'],
+            ['userID' => '8','name' => 'Wahab','email' => 'wahab','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => 'civil'],
+            ['userID' => '9','name' => 'Rahim','email' => 'rahim','lastLogin' => NULL,'status' => '1','lastLoginIP' => NULL,'password' => \Illuminate\Support\Facades\Hash::make('admin123'),'department_id' => '1','branch_id' => '1','is_signing_authority' => '0','arm_designation' => 'civil']
         ];
-
         foreach ($aryUsers as $user) {
             DB::table('users')->insert([
                 'name' => $user['name'],
